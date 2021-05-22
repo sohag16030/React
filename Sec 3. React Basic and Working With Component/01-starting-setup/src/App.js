7a8191438c5e8202b48494a10cde6cce35f4e901
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-05-22 17:59:00 +0600

    updated

--- Sec 3. React Basic and Working With Component/01-starting-setup/src/App.js
+++ Sec 3. React Basic and Working With Component/01-starting-setup/src/App.js
@@ -1,7 +1,12 @@
 function App() {
+  //document.getElementById('root').innerHTML = 'This is also visible';
+  const para = document.createElement('p');
+  para.textContent = `this is also visible`; 
+  document.getElementById('root').append(para);
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p> This is also visible</p>
     </div>
   );
 }
