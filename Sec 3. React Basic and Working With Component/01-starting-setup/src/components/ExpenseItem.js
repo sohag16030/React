8ee93ecf935626dc201580342771f32d54b7c99f
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-05-24 08:31:18 +0600

    udpated

--- Sec 3. React Basic and Working With Component/01-starting-setup/src/components/ExpenseItem.js
+++ Sec 3. React Basic and Working With Component/01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>March 28th</div>
+      <div>{}</div>
       <div className="expense-item__description">
-        <h2 className="expense-item h2 ">Car Insurance</h2>
+        <h2 className="expense-item h2 ">{}</h2>
       </div>
-      <div className="expense-item__price">$294.67</div>
+      <div className="expense-item__price">{}</div>
     </div>
   );
 }
