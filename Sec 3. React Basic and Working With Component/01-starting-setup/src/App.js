9b7a7ef7ac84094ba7a71681efbd56e1e4ff146f
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-05-25 23:07:51 +0600

    split one component into multiple component

--- Sec 3. React Basic and Working With Component/01-starting-setup/src/App.js
+++ Sec 3. React Basic and Working With Component/01-starting-setup/src/App.js
@@ -5,7 +5,7 @@ function App() {
       id: "e1",
       title: "Car Insurance",
       amount: 267.67,
-      date: new Date(2021,11, 9),
+      date: new Date(2021, 11, 9),
     },
     {
       id: "e2",
@@ -27,17 +27,17 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
