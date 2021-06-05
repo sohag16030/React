c4e76ca98a2bf9bf996b1b60e06730bb1c5919e5
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-06-05 14:22:27 +0600

    updated

--- Sec 3. React Basic and Working With Component/01-starting-setup/src/components/ExpenseItem.js
+++ Sec 3. React Basic and Working With Component/01-starting-setup/src/components/ExpenseItem.js
@@ -1,16 +1,15 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "./Card"
 function ExpenseItem(props) {
   return (
-    <div className="card">
-      <div className="expense-item">
+      <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
           <h2 className="expense-item h2 ">{props.title}</h2>
         </div>
         <div className="expense-item__price">{props.amount}</div>
-      </div>
-    </div>
+      </Card>  
   );
 }
 export default ExpenseItem;
