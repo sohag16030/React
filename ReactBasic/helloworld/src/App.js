3a613031985e248d4c0e6ee02b999316dc204e63
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-06-05 15:46:35 +0600

    ReactBasic Started

---
+++ ReactBasic/helloworld/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <div className="App">
+      <Hello/>
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactBasic/helloworld/src/components/Hello.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/* function Hello(){ //default export
+    return (
+        <h1>Hello World</h1>
+    )
+}
+export default Hello; */
+
+/* export const User = function(){ //Name export
+    return (
+        <h1>Hello User</h1>
+    )
+} */
+
+export  const Hello =()=>{
+    return (
+        <h2>Hello world</h2>
+    )
+}
