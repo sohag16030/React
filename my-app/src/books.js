d01cfe6d353c135d73d2939beec52c5843637aa8
sohag16030 <shohag.mbstu.it16030@gmail.com>
2021-12-08 22:48:21 +0600

    updated

--- my-app/src/books.js
+++ my-app/src/books.js
@@ -1,9 +1,9 @@
 export const data = [
   {
     id: 1,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71w7zp2lZ3L._AC_UL200_SR200,200_.jpg',
-    title: 'All American Christmas',
-    author: 'Rachel Campos-Duffy',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91aNPxKIruL._AC_UL200_SR200,200_.jpg',
+    title: 'The 1619 Project: A New Origin Story',
+    author: 'Nikole Hannah-Jones',
   },
   {
     id: 2,
@@ -16,5 +16,23 @@ export const data = [
     img: 'https://images-na.ssl-images-amazon.com/images/I/71bROORU%2BnL._AC_UL200_SR200,200_.jpg',
     title: 'The Christmas Pig',
     author: 'J. K. Rowling',
+  },
+  {
+    id: 4,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91io14GlrxL._AC_UL200_SR200,200_.jpg',
+    title: 'Call Us What We Carry: Poems',
+    author: 'Amanda Gorman',
+  },
+  {
+    id: 5,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81xWWojXXyL._AC_UL200_SR200,200_.jpg',
+    title: 'Cat Kid Comic Club: Perspectives: A Graphic',
+    author: 'Dav Pilkey',
+  },
+  {
+    id: 6,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81pLHdMtwdS._AC_UL200_SR200,200_.jpg',
+    title: 'Go Tell the Bees That I Am Gone: A Novel',
+    author: 'Diana Gabaldon',
   },
 ]
